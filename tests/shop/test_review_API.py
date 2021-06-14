6f5708fb22f6e03a814bc10fb76dced132338586
Atya <nushnushka@gmail.com>
2021-06-14 22:16:51 +0300

    -

--- tests/shop/test_review_API.py
+++ tests/shop/test_review_API.py
@@ -5,7 +5,7 @@ from rest_framework.status import HTTP_200_OK
 
 # проверка получения списка отзывов
 @pytest.mark.django_db
-def test_collection_list(client, review_factory):
+def test_review_list(client, review_factory):
     # создаём десять отзывов
     review = review_factory(_quantity=10)
     url = reverse("product-reviews-list")
